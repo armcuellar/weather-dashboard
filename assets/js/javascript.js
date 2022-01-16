947e953f7f9b4f1cf0dc1ad199298c2cc41b043d
Armando <cuelarmando@yahoo.com>
2022-01-16 11:16:54 -0500

    added weather icon

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,6 +3,7 @@ var currentWeatherEl = document.querySelector(".currentWeather");
 var fiveDayWeatherEl = document.querySelector(".fiveDayWeather");
 var cityInputEl = document.querySelector("#city");
 
+// function to obtain city's coordinates
 var searchCity = function (event) {
     event.preventDefault();
     console.log(cityInputEl.value);
@@ -20,20 +21,99 @@ var searchCity = function (event) {
                     var lat = data.coord.lat;
                     var lon = data.coord.lon;
 
-                    cityWeather(lat, lon);
+                    cityWeather(city, lat, lon);
                 });
             }
             else {
-                alert("City Not Found");
+                alert("City Not Found, enter a US City");
             }
         })
         .catch(function (error) {
-            alert("There is an error");
+            alert("There is an error obtaining data");
         });
+    cityInputEl.placeholder = city;
+    cityInputEl.value = "";
 }
-var cityWeather = function (latt, long) {
+
+// function to use city coordinates to get weather data
+var cityWeather = function (city, latt, long) {
     var weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latt + "&lon=" + long + "&appid=cc93eb9259444b15987361c38b14835c";
-    console.log(weatherApi);
+
+    currentWeatherEl.textContent = "";
+
+    fetch(weatherApi)
+        .then(function (response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then(function (data) {
+                    var date = city.toUpperCase() + " " + moment.unix(data.current.dt).format("MMM Do, YYYY");
+
+                    var cityTitle = document.createElement("h2");
+                    cityTitle.textContent = date;
+
+                    // Checks weather code and returns icon image
+                    var weatherCode = data.current.weather[0].id;
+
+                    if (200 <= weatherCode && weatherCode <= 232) {
+                        var icon = "11d";
+                    }
+                    else if (300 <= weatherCode && weatherCode <= 321) {
+                        var icon = "09d";
+                    }
+                    else if (500 <= weatherCode && weatherCode <= 504) {
+                        var icon = "10d";
+                    }
+                    else if (weatherCode == 511) {
+                        var icon = "13d";
+                    }
+                    else if (520 <= weatherCode && weatherCode <= 531) {
+                        var icon = "09d";
+                    }
+                    else if (600 <= weatherCode && weatherCode <= 622) {
+                        var icon = "13d";
+                    }
+                    else if (701 <= weatherCode && weatherCode <= 781) {
+                        var icon = "50d";
+                    }
+                    else if (weatherCode == 800) {
+                        var icon = "01d";
+                    }
+                    else if (weatherCode == 801) {
+                        var icon = "02d";
+                    }
+                    else if (weatherCode == 802) {
+                        var icon = "03d";
+                    }
+                    else if (803 <= weatherCode && weatherCode <= 804) {
+                        var icon = "04d";
+                    }
+
+                    var weatherIcon = document.createElement("img");
+                    weatherIcon.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+                    // end of weather icon code 
+
+                    currentWeatherEl.appendChild(cityTitle);
+                    currentWeatherEl.appendChild(weatherIcon);
+
+                    console.log(weatherCode);
+
+                    console.log(weatherApi);
+                    console.log(date);
+
+                });
+            }
+            else {
+                alert("No current Weather Data");
+            }
+        })
+        .catch(function (error) {
+            alert("There is an error obtaining data");
+        });
+
+
+
+
+
 }
 
 citySearchEl.addEventListener("submit", searchCity);
