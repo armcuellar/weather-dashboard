d00015631070f53af6d0f0c53a28655f1371b06e
Armando <cuelarmando@yahoo.com>
2022-01-16 16:39:34 -0500

    added bootstrap and custom style to weather dashboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,12 +17,12 @@ var searchCity = function (event) {
 
     fetch(searchCityApi)
         .then(function (response) {
-            // request was successful
             if (response.ok) {
                 response.json().then(function (data) {
                     var lat = data.coord.lat;
                     var lon = data.coord.lon;
 
+                    // uses coordinate to get current and future forecast as well as saves them
                     cityWeather(city, lat, lon);
                     recentSearch(city, lat, lon);
                     saveSearch(city, lat, lon);
@@ -35,27 +35,30 @@ var searchCity = function (event) {
         .catch(function (error) {
             alert("There is an error obtaining data");
         });
-    cityInputEl.placeholder = city;
-    cityInputEl.value = "";
+
 }
 
 // function to use city coordinates to get weather data
 var cityWeather = function (city, latt, long) {
-
+    cityInputEl.placeholder = city;
+    cityInputEl.value = "";
+    // api call
     var weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latt + "&lon=" + long + "&units=imperial&appid=cc93eb9259444b15987361c38b14835c";
 
     currentWeatherEl.textContent = "";
 
     fetch(weatherApi)
         .then(function (response) {
-            // request was successful
             if (response.ok) {
                 response.json().then(function (data) {
                     var current = data.current;
-                    var date = city.toUpperCase() + " " + moment.unix(current.dt).format("ddd MMM Do, YYYY");
+                    var date = moment.unix(current.dt).format("ddd MMM Do, YYYY");
 
                     var cityTitle = document.createElement("h2");
-                    cityTitle.textContent = date;
+                    cityTitle.textContent = city.toUpperCase();
+
+                    var cityDate = document.createElement("h2");
+                    cityDate.textContent = date;
 
                     var weatherIcon = document.createElement("img");
                     weatherIcon.src = "http://openweathermap.org/img/wn/" + current.weather[0].icon + "@2x.png";
@@ -67,10 +70,23 @@ var cityWeather = function (city, latt, long) {
                     windText.textContent = "Wind: " + current.wind_speed + " MPH";
                     var humidityText = document.createElement("p");
                     humidityText.textContent = "Humidity: " + current.humidity + " %";
+
                     var uvIndexText = document.createElement("p");
                     uvIndexText.textContent = "UV Index: " + current.uvi;
 
+                    if (current.uvi < 2) {
+                        uvIndexText.className = "bg-success"
+                    }
+                    else if (current.uvi > 7) {
+                        uvIndexText.className = "bg-danger"
+                    }
+                    else {
+                        uvIndexText.className = "bg-warning"
+                    }
+
+
                     currentWeatherEl.appendChild(cityTitle);
+                    currentWeatherEl.appendChild(cityDate);
                     currentWeatherEl.appendChild(weatherIcon);
                     currentWeatherEl.appendChild(tempText);
                     currentWeatherEl.appendChild(windText);
@@ -89,21 +105,25 @@ var cityWeather = function (city, latt, long) {
         });
 }
 
+// function to obtain and display the five day forecast
 var futureWeather = function (data) {
     console.log(data.daily);
     fiveDayWeatherEl.textContent = ""
 
     var futureData = data.daily;
 
-    // checks 5 day forcast
+    // gets 5 day forcast
     for (i = 1; i < 6; i++) {
         var date = moment.unix(data.daily[i].dt).format("ddd MMM Do, YYYY");
 
+        var dayContainer = document.createElement("div")
+        dayContainer.classList = "col-lg-2 bg-secondary text-white"
         var cityTitle = document.createElement("h3");
         cityTitle.textContent = date;
 
         var weatherIcon = document.createElement("img");
         weatherIcon.src = "http://openweathermap.org/img/wn/" + futureData[i].weather[0].icon + "@2x.png";
+
 
         var tempText = document.createElement("p");
         tempText.textContent = "Temp: " + futureData[i].temp.day + " °F";
@@ -112,15 +132,18 @@ var futureWeather = function (data) {
         var humidityText = document.createElement("p");
         humidityText.textContent = "Humidity: " + futureData[i].humidity + " %";
 
-        fiveDayWeatherEl.appendChild(cityTitle);
-        fiveDayWeatherEl.appendChild(weatherIcon);
-        fiveDayWeatherEl.appendChild(tempText);
-        fiveDayWeatherEl.appendChild(windText);
-        fiveDayWeatherEl.appendChild(humidityText);
+        dayContainer.appendChild(cityTitle);
+        dayContainer.appendChild(weatherIcon);
+        dayContainer.appendChild(tempText);
+        dayContainer.appendChild(windText);
+        dayContainer.appendChild(humidityText);
+
+        fiveDayWeatherEl.appendChild(dayContainer);
 
     }
 
 }
+// function to save search to local array
 var saveSearch = function (cityName, latitude, longitude) {
 
     var cityObj = {
@@ -131,16 +154,20 @@ var saveSearch = function (cityName, latitude, longitude) {
     cityInfo.push(cityObj)
     save();
 }
+// function to save to local storage
 var save = function () {
     localStorage.setItem("weatherCity", JSON.stringify(cityInfo));
 }
+// function to create recent search list
 var recentSearch = function (cityName, latitude, longitude) {
     var recentCity = document.createElement("button");
     recentCity.textContent = cityName;
+    recentCity.classList = "p-2 bd-highlight"
     recentCity.lat = latitude;
     recentCity.lon = longitude;
-    recentSearchEl.append(recentCity);
+    recentSearchEl.prepend(recentCity);
 }
+// function to retrieve weather forecast from saved city
 var retrieveCity = function (event) {
     var city = event.target.textContent;
     var lat = event.target.lat;
@@ -148,6 +175,7 @@ var retrieveCity = function (event) {
 
     cityWeather(city, lat, lon);
 }
+// function to load recent search list from local storage
 var load = function () {
     var searchHistory = localStorage.getItem("weatherCity")
     if (!searchHistory) {
@@ -163,7 +191,10 @@ var load = function () {
 
         recentSearch(city, lat, lon);
     }
+
+
 }
+
 citySearchEl.addEventListener("submit", searchCity);
 recentSearchEl.addEventListener("click", retrieveCity);
 load();
